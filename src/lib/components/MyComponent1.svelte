48e56eb5c75d406e1aee3484d764f34c267d3674
Kunal Shah <shahkunal9@gmail.com>
2024-07-18 11:08:20 +0800

    first commit

---
+++ src/lib/components/MyComponent1.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	type ComponentProps = {
+		disabled?: boolean;
+		label: string;
+	};
+
+	let { disabled = false, label }: ComponentProps = $props();
+</script>
+
+<div>
+	{#if disabled}
+		<button disabled>{label}</button>
+	{:else}
+		<button>{label}</button>
+	{/if}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import MyComponent1 from '$lib/components/MyComponent1.svelte';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<MyComponent1 label="Hello, world!" disabled={true} />
